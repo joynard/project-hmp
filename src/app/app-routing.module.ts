d9259a566f690e1b33a10e902c1de9ddb0cc5e4f
Alexander Lapod <alexanderfabian18@gmail.com>
2025-09-30 14:39:33 +0700

    Adding Profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'tabs',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then( m => m.ProfilePageModule)
   }
 ];
 
